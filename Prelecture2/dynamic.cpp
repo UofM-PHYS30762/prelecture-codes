c5e75d5aac830e1fb70b85bdacab46b20cc3e662
Charanjit Kaur Khosa <khosacharanjit@gmail.com>
2024-02-03 15:20:24 +0000

    updated dynamic.cpp

--- Prelecture2/dynamic.cpp
+++ Prelecture2/dynamic.cpp
@@ -1,5 +1,5 @@
 // PL2/dynamic.cpp
-// Example of program using dynamic memory allocation
+// Example of program using dynamic memory allocation (PHYS30762)
 // Source: Prof. Niels Walet's code repository
 #include<iostream>
 int main()
@@ -8,18 +8,20 @@ int main()
   std::cout<<"Enter the number of values to enter: ";
   std::cin>>number_of_values;
   std::cout<<"You would like to enter "<<number_of_values<<" values"<<std::endl;
-  double *values = new double[number_of_values];
+  double* values = new double[number_of_values];
   std::cout<<"Allocated memory for "<<number_of_values<<" doubles"<<std::endl;
   std::cout<<"Enter "<<number_of_values<<" doubles"<<std::endl;
-  for(int i{};i<number_of_values;i++) {
-    std::cin>>values[i];
+  for(int i{}; i<number_of_values; i++) 
+    {
+      std::cin>>values[i];
     }
   std::cout<<"Here are your values: ";
-  for(int i{};i<number_of_values;i++) {
-    std::cout<<values[i]<<" ";
+  for(int i{}; i<number_of_values; i++) 
+    {
+      std::cout<<values[i]<<" ";
     }
   std::cout<<std::endl;
-  // Free memory
+  //Free memory
   delete[] values;
   std::cout<<"Freed memory"<<std::endl;
   return 0;
