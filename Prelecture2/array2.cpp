f29c22c29992e65a3601d0284b9c2ae7a3ec6e8c
Charanjit Kaur Khosa <khosacharanjit@gmail.com>
2024-02-03 15:21:11 +0000

    Add files via upload

--- Prelecture2/array2.cpp
+++ Prelecture2/array2.cpp
@@ -3,10 +3,10 @@
 //Source: Prof. Niels Walet's code repository
 //Last updated: Charanjit Kaur, 31/01/24
 #include<iostream> 
-void print_array(const int array_entries, int *array) 
+void print_array(const int array_entries, int* array) 
 {
   // show that array[i] is equivalent to *(array+i) 
-  for(int i{};i<array_entries;i++) 
+  for(int i{}; i<array_entries; i++) 
     std::cout<<array[i]<<" "<<*(array+i)<<std::endl;
   return; 
 }
@@ -14,7 +14,8 @@ int main()
 { 
   const int array_entries{3}; 
   int array[array_entries]; 
-  for(int i{};i<array_entries;i++) array[i]=i; 
+  for(int i{}; i<array_entries; i++) array[i]=i; 
   print_array(array_entries,array); 
   return 0; 
 }
+
