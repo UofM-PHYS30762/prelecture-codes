9020c89cfdab5dfa2978438aaf4e8ad293e92f69
Charanjit Kaur Khosa <khosacharanjit@gmail.com>
2024-02-10 12:05:41 +0000

    Add files via upload

---
+++ Prelecture3/cstringstream.cpp
@@ -0,0 +1,14 @@
+// PL3/cstringstream.cpp
+// Demonstration  a string stream in the "C" syntax
+// Source: Prof. Niels Walet's code repository
+// Last updated: Charanjit Kaur, 07/02/24
+#include<iostream>
+#include<cstring>
+int main() 
+{
+  char output_filename[100];
+  int file_index{123};
+  sprintf(output_filename,"FileData.%d",file_index);
+  std::cout<<output_filename<<std::endl;
+  return 0;
+}
